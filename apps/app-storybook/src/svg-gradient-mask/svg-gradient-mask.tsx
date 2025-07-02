15ac79a236e46a1a5dc6fdde41231968895c724d
reekystive <reekystive@gmail.com>
2025-07-02 15:15:06 +0800

    feat: ✨ enhance SvgGradientMask with static and animated masks, including invert filter functionality

--- apps/app-storybook/src/svg-gradient-mask/svg-gradient-mask.tsx
+++ apps/app-storybook/src/svg-gradient-mask/svg-gradient-mask.tsx
@@ -18,43 +18,28 @@ export const SvgGradientMask: FC = () => {
 
   const animatedGradientId = `${id}-animated-gradient`;
   const animatedMaskId = `${id}-animated-mask`;
+  const invertFilterId = `${id}-invert-filter`;
+  const invertMaskId = `${id}-invert-mask`;
+  const invertAnimatedMaskId = `${id}-invert-animated-mask`;
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      {/* defs */}
-      <svg className="absolute size-20" viewBox="0 0 1 1">
-        <defs>
-          <linearGradient gradientUnits="objectBoundingBox" id={gradientId} x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="40%" stopColor="white" />
-            <stop offset="60%" stopColor="black" />
-          </linearGradient>
-
-          <mask maskContentUnits="objectBoundingBox" id={maskId}>
-            <rect width="100%" height="100%" fill={`url(#${gradientId})`} />
-          </mask>
-
-          <linearGradient gradientUnits="objectBoundingBox" id={animatedGradientId} x1="0%" y1="0%" x2="0%" y2="100%">
-            <motion.stop
-              initial={{ offset: '-20%' }}
-              animate={{ offset: '100%' }}
-              transition={{ type: 'tween', ease: 'easeOut', duration: 3, repeat: Infinity, repeatDelay: 1 }}
-              stopColor="white"
-            />
-            <motion.stop
-              initial={{ offset: '0%' }}
-              animate={{ offset: '120%' }}
-              transition={{ type: 'tween', ease: 'easeOut', duration: 3, repeat: Infinity, repeatDelay: 1 }}
-              stopColor="black"
-            />
-          </linearGradient>
-
-          <mask maskContentUnits="objectBoundingBox" id={animatedMaskId}>
-            <rect width="100%" height="100%" fill={`url(#${animatedGradientId})`} />
-          </mask>
-        </defs>
-      </svg>
-
+    <div className="items-center-safe flex min-h-screen flex-col justify-center gap-6 py-6">
+      {/* static svg mask */}
+      <h2 className="-mb-2 text-lg font-bold uppercase">Static Svg Mask</h2>
       <div className="relative flex flex-row items-center justify-center gap-4 bg-neutral-900 p-8">
+        <svg className="absolute size-20" viewBox="0 0 1 1">
+          <defs>
+            <linearGradient gradientUnits="objectBoundingBox" id={gradientId} x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="40%" stopColor="white" />
+              <stop offset="60%" stopColor="black" />
+            </linearGradient>
+
+            <mask maskContentUnits="objectBoundingBox" id={maskId}>
+              <rect width="100%" height="100%" fill={`url(#${gradientId})`} />
+            </mask>
+          </defs>
+        </svg>
+
         <svg className="h-[400px] w-[200px]" viewBox="0 0 100 200">
           <rect width="100%" height="100%" fill={`url(#${gradientId})`} />
         </svg>
@@ -65,18 +50,99 @@ export const SvgGradientMask: FC = () => {
 
         <div
           className="h-[400px] w-[200px] overflow-hidden bg-red-200 p-2 dark:bg-red-900"
-          style={{
-            maskImage: `url(#${maskId})`,
-          }}
+          style={{ maskImage: `url(#${maskId})` }}
+        >
+          {LOREM_IPSUM}
+        </div>
+      </div>
+
+      {/* static svg mask with invert filter */}
+      <h2 className="-mb-2 text-lg font-bold uppercase">Static Svg Mask with Invert Filter</h2>
+      <div className="relative flex flex-row items-center justify-center gap-4 bg-neutral-900 p-8">
+        <svg className="absolute size-20" viewBox="0 0 1 1">
+          <defs>
+            <filter id={invertFilterId} colorInterpolationFilters="sRGB">
+              <feColorMatrix
+                type="matrix"
+                values="
+              -1 0 0 0 1
+              0 -1 0 0 1
+              0 0 -1 0 1
+              0 0 0 1 0"
+              />
+            </filter>
+
+            <mask maskContentUnits="objectBoundingBox" id={invertMaskId}>
+              <rect width="100%" height="100%" fill={`url(#${gradientId})`} filter={`url(#${invertFilterId})`} />
+            </mask>
+          </defs>
+        </svg>
+
+        <svg className="h-[400px] w-[200px]" viewBox="0 0 100 200">
+          <rect width="100%" height="100%" className="fill-amber-600" mask={`url(#${maskId})`} />
+        </svg>
+
+        <svg className="h-[400px] w-[200px]" viewBox="0 0 100 200">
+          <rect width="100%" height="100%" className="fill-amber-600" mask={`url(#${invertMaskId})`} />
+        </svg>
+
+        <div
+          className="h-[400px] w-[200px] overflow-hidden bg-red-200 p-2 dark:bg-red-900"
+          style={{ maskImage: `url(#${invertMaskId})` }}
+        >
+          {LOREM_IPSUM}
+        </div>
+      </div>
+
+      {/* animated svg mask (animate stops) */}
+      <h2 className="-mb-2 text-lg font-bold uppercase">Animated Svg Mask (Animate stops)</h2>
+      <div className="relative flex flex-row items-center justify-center gap-4 bg-neutral-900 p-8">
+        <svg className="absolute size-20" viewBox="0 0 1 1">
+          <defs>
+            <linearGradient gradientUnits="objectBoundingBox" id={animatedGradientId} x1="0%" y1="0%" x2="0%" y2="100%">
+              <motion.stop
+                initial={{ offset: '-20%' }}
+                animate={{ offset: '100%' }}
+                transition={{ type: 'tween', ease: 'easeOut', duration: 3, repeat: Infinity, repeatDelay: 1 }}
+                stopColor="white"
+              />
+              <motion.stop
+                initial={{ offset: '0%' }}
+                animate={{ offset: '120%' }}
+                transition={{ type: 'tween', ease: 'easeOut', duration: 3, repeat: Infinity, repeatDelay: 1 }}
+                stopColor="black"
+              />
+            </linearGradient>
+
+            <mask maskContentUnits="objectBoundingBox" id={animatedMaskId}>
+              <rect width="100%" height="100%" fill={`url(#${animatedGradientId})`} />
+            </mask>
+
+            <mask maskContentUnits="objectBoundingBox" id={invertAnimatedMaskId}>
+              <rect
+                width="100%"
+                height="100%"
+                fill={`url(#${animatedGradientId})`}
+                filter={`url(#${invertFilterId})`}
+              />
+            </mask>
+          </defs>
+        </svg>
+
+        <svg className="h-[400px] w-[200px]" viewBox="0 0 100 200">
+          <rect width="100%" height="100%" className="fill-amber-600" mask={`url(#${animatedMaskId})`} />
+        </svg>
+
+        <div
+          className="h-[400px] w-[200px] overflow-hidden bg-red-200 p-2 dark:bg-red-900"
+          style={{ maskImage: `url(#${animatedMaskId})` }}
         >
           {LOREM_IPSUM}
         </div>
 
         <div
           className="h-[400px] w-[200px] overflow-hidden bg-red-200 p-2 dark:bg-red-900"
-          style={{
-            maskImage: `url(#${animatedMaskId})`,
-          }}
+          style={{ maskImage: `url(#${invertAnimatedMaskId})` }}
         >
           {LOREM_IPSUM}
         </div>
