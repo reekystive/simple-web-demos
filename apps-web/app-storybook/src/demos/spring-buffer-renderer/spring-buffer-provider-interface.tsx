c98dec8bbc24de31f0ae36298b356a63741c685d
reekystive <reekystive@gmail.com>
2025-11-06 19:27:06 +0800

    ♻️ refactor: move useSpringBuffer logic to a separate file

---
+++ apps-web/app-storybook/src/demos/spring-buffer-renderer/spring-buffer-provider-interface.tsx
@@ -0,0 +1,21 @@
+import { MotionValue } from 'motion/react';
+
+export interface SpringBufferContextValue {
+  // params
+  visualDuration: number;
+  setVisualDuration: (v: number) => void;
+
+  // motion values
+  contentMV: MotionValue<string>;
+  contentGraphemeSegmentsMV: MotionValue<string[]>;
+  contentGraphemeLengthMV: MotionValue<number>;
+  cursorGraphemeIndexSpringMV: MotionValue<number>;
+  cursorUTF16IndexSpringMV: MotionValue<number>;
+  renderedValueSpringMV: MotionValue<string>;
+  bufferValueSpringMV: MotionValue<string>;
+
+  // actions
+  append: (suffix: string) => void;
+  flush: () => void;
+  clear: () => void;
+}
---
+++ apps-web/app-storybook/src/demos/spring-buffer-renderer/use-spring-buffer.tsx
@@ -0,0 +1,127 @@
+import { animate, ValueTransition } from 'motion';
+import { useMotionValue, useTransform } from 'motion/react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { SPRING_PARAMS } from './constants.js';
+import { segmentGraphemes } from './segmenter.js';
+import { SpringBufferContextValue } from './spring-buffer-provider-interface.js';
+
+export const useSpringBuffer = (): SpringBufferContextValue => {
+  const [visualDuration, setVisualDurationInternal] = useState<number>(SPRING_PARAMS.VISUAL_DURATION.DEFAULT);
+
+  // motion values
+  const contentMV = useMotionValue<string>('');
+  const contentGraphemeSegmentsMV = useTransform(() => segmentGraphemes(contentMV.get()));
+  const contentGraphemeLengthMV = useTransform(() => {
+    return contentGraphemeSegmentsMV.get().length;
+  });
+
+  // MARK: ANIMATION DRIVER
+  const cursorGraphemeIndexSpringMVRaw = useMotionValue<number>(contentGraphemeLengthMV.get());
+
+  // eslint-disable-next-line react-hooks/refs
+  contentGraphemeLengthMV.on('change', () =>
+    retune({ type: 'spring', bounce: 0, restDelta: 0.1, restSpeed: 0.01, visualDuration: visualDuration })
+  );
+
+  const cursorGraphemeIndexSpringMV = useTransform(() => {
+    // Math.min(Math.round(cursorGraphemeIndexSpringMVRaw.get()), contentGraphemeLengthMV.get())
+    return Math.round(cursorGraphemeIndexSpringMVRaw.get());
+  });
+
+  const cursorUTF16IndexSpringMV = useTransform(() => {
+    return contentGraphemeSegmentsMV.get().slice(0, cursorGraphemeIndexSpringMV.get()).join('').length;
+  });
+
+  const renderedValueSpringMV = useTransform(() => {
+    return contentMV.get().slice(0, cursorUTF16IndexSpringMV.get());
+  });
+
+  const bufferValueSpringMV = useTransform(() => {
+    return contentMV.get().slice(cursorUTF16IndexSpringMV.get());
+  });
+
+  // MARK: ACTIONS
+
+  const controlsRef = useRef<ReturnType<typeof animate> | null>(null);
+
+  const retune = useCallback(
+    (nextConfig: ValueTransition) => {
+      const vel = cursorGraphemeIndexSpringMVRaw.getVelocity() || 0;
+      controlsRef.current?.stop();
+      controlsRef.current = animate(cursorGraphemeIndexSpringMVRaw, contentGraphemeLengthMV.get(), {
+        ...nextConfig,
+        velocity: vel, // keep current velocity
+      });
+    },
+    [contentGraphemeLengthMV, cursorGraphemeIndexSpringMVRaw]
+  );
+
+  useEffect(() => () => controlsRef.current?.stop(), []);
+
+  const setVisualDuration = useCallback(
+    (v: number) => {
+      setVisualDurationInternal(v);
+      retune({ type: 'spring', bounce: 0, restDelta: 0.1, restSpeed: 0.01, visualDuration: v });
+    },
+    [retune]
+  );
+
+  const append = useCallback(
+    (suffix: string) => {
+      const newContent = contentMV.get() + suffix;
+      contentMV.set(newContent);
+    },
+    [contentMV]
+  );
+
+  const flush = useCallback(() => {
+    const vel = cursorGraphemeIndexSpringMVRaw.getVelocity() || 0;
+    retune({
+      type: 'inertia',
+      velocity: vel,
+      restDelta: 0.1,
+      restSpeed: 0.01,
+      timeConstant: 150,
+      modifyTarget: () => contentGraphemeLengthMV.get(),
+    });
+  }, [contentGraphemeLengthMV, cursorGraphemeIndexSpringMVRaw, retune]);
+
+  const clear = useCallback(() => {
+    contentMV.jump('');
+    contentGraphemeSegmentsMV.jump([]);
+    contentGraphemeLengthMV.jump(0);
+    cursorGraphemeIndexSpringMVRaw.jump(0);
+    cursorGraphemeIndexSpringMV.jump(0);
+    cursorUTF16IndexSpringMV.jump(0);
+    renderedValueSpringMV.jump('');
+    bufferValueSpringMV.jump('');
+  }, [
+    contentMV,
+    contentGraphemeSegmentsMV,
+    contentGraphemeLengthMV,
+    cursorGraphemeIndexSpringMV,
+    cursorUTF16IndexSpringMV,
+    renderedValueSpringMV,
+    bufferValueSpringMV,
+    cursorGraphemeIndexSpringMVRaw,
+  ]);
+
+  const value: SpringBufferContextValue = {
+    visualDuration,
+    setVisualDuration,
+
+    contentMV,
+    contentGraphemeSegmentsMV,
+    contentGraphemeLengthMV,
+    cursorGraphemeIndexSpringMV,
+    cursorUTF16IndexSpringMV,
+    renderedValueSpringMV,
+    bufferValueSpringMV,
+
+    append,
+    flush,
+    clear,
+  };
+
+  return value;
+};
