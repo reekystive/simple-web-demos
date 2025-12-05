bbd80925ee3d91e53745c74baa3027c44cf65b1a
reekystive <reekystive@gmail.com>
2025-12-05 14:36:53 +0800

    ✨ fix: disable eslint rule for purity in useFaker hook to allow for random seed generation

--- apps-web/app-storybook/src/demos/scroll-anchoring/hooks/use-faker.ts
+++ apps-web/app-storybook/src/demos/scroll-anchoring/hooks/use-faker.ts
@@ -2,6 +2,7 @@ import { Faker, en } from '@faker-js/faker';
 import { useMemo } from 'react';
 
 export const useFaker = () => {
+  // eslint-disable-next-line react-hooks/purity
   const seed = useMemo(() => Math.floor(Math.random() * 1000000), []);
   const fakerWithSeed = useMemo(() => new Faker({ locale: [en], seed }), [seed]);
   const faker = useMemo(() => new Faker({ locale: [en] }), []);
